dbd11bb6f76ec99cbbdbc549fcd316c41d9ec9ba
Anusha H N <anushahn2905@gmail.com>
2026-04-06 14:17:09 +0530

    updating

--- src/app/dashboard/Menu/settings/settings.component.ts
+++ src/app/dashboard/Menu/settings/settings.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-settings',
   imports: [],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.css',
+  styleUrl: './settings.component.css',
 })
 export class Settings {
 
